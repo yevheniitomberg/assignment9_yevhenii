fb88e7ad379b2c3f17132c3b8baa1bd1fd5cb6b2
Yevhenii Tomberg <yevhenii@tomberg.com>
2022-11-09 04:47:16 +0300

    Add project files.

---
+++ assignment9_yevhenii/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace assignment9_yevhenii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.RichTextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(73, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the text to find most frequent char";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(109, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.inputBox.Location = new System.Drawing.Point(42, 41);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(354, 149);
+            this.inputBox.TabIndex = 4;
+            this.inputBox.Text = "";
+            // 
+            // labelResult
+            // 
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelResult.Location = new System.Drawing.Point(42, 203);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(354, 155);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnClear.Location = new System.Drawing.Point(227, 376);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 30);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 418);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Most Frequent Character";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox inputBox;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ assignment9_yevhenii/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment9_yevhenii
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly Regex sWhitespace = new Regex(@"\s+");
+        private static int numOfSpaces;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static List<int> findIdxs(string s, char f)
+        {
+            var foundIndexes = new List<int>();
+            for (int i = s.IndexOf(f); i > -1; i = s.IndexOf(f, i + 1))
+            {
+                foundIndexes.Add(i);
+            }
+            return foundIndexes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<KeyValuePair<char, int>> list = getResult(this.inputBox.Text);
+            Random rng = new Random();
+            this.labelResult.Text += "Num of spaces: " + numOfSpaces + "\n";
+            foreach (KeyValuePair<char, int> kvp in list)
+            {
+                this.labelResult.Text += "Most frequent character is: " + kvp.Key + " ("+kvp.Value+")\n";
+                Color color = Color.FromArgb(rng.Next(-16777216, 0)); 
+                List<int> idxs = findIdxs(this.inputBox.Text.ToLower(), kvp.Key);
+                foreach (int idx in idxs)
+                {
+                    this.inputBox.Select(idx, 1);
+                    this.inputBox.SelectionColor = color;
+                    this.inputBox.SelectionFont = new Font(this.inputBox.Font, FontStyle.Bold);
+                }
+            }
+            this.inputBox.Enabled = false;
+            this.button1.Enabled = false;
+        }
+
+        private static List<KeyValuePair<char, int>> getResult(string str) 
+        { 
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            numOfSpaces = findIdxs(str, ' ').ToArray().Length;
+            
+            foreach (char c in sWhitespace.Replace(str.ToLower(), ""))
+            {
+                try
+                {
+                    dict[c]++;
+                } 
+                catch
+                {
+                    dict[c] = 1;
+                }
+            }
+
+            List<KeyValuePair<char, int>> list = new List<KeyValuePair<char, int>>();
+
+            int tempInt = 0;
+            char tempChar = ' ';
+
+
+            foreach (KeyValuePair<char, int> kvp in dict)
+            {
+                if (kvp.Value > tempInt)
+                {
+                    tempInt = kvp.Value;
+                    tempChar = kvp.Key;
+                }
+            }
+
+            list.Add(new KeyValuePair<char, int>(tempChar, tempInt));
+
+            foreach (KeyValuePair<char, int> kvp in dict)
+            {
+                if (kvp.Value == list.First().Value && !kvp.Key.Equals(list.First().Key))
+                {
+                    list.Add(kvp);
+                }
+            }
+            return list;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.inputBox.Text = "";
+            this.inputBox.Enabled = true;
+            this.labelResult.Text = "";
+            this.button1.Enabled = true;
+        }
+    }
+}
